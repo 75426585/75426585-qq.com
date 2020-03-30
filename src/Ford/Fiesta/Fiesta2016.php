251e1626835c6baa8c259359ec89c6afe3c1e038
ji <ji@jideMacBook-Pro-2.local>
2020-03-30 17:43:15 +0800

    3

---
+++ src/Ford/Fiesta/Fiesta2016.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ford\Fiesta;
+
+class Fiesta2016 {
+    public function info() {
+        echo "This is Ford Fiesta2016!<br>";
+    }
+}
---
+++ src/Ford/Fusion/Fusion2016.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Ford\Fusion;
+class Fusion2016 {
+    public function info() {
+        echo 'This is Ford Fusion2016 !<br>';
+    }
+}
